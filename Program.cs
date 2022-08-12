c565521f0962006796f4a865bedc3e8f27f47255
kden <ks230den@gmail.com>
2022-08-12 14:53:41 +0300

    Исправление ошибок в представлении добавления и редактирования записи

--- Program.cs
+++ Program.cs
@@ -15,9 +15,7 @@ builder.Services.AddControllersWithViews();
 builder.Services.AddScoped<IPersonRepository, PersonRepository>();
 builder.Services.AddScoped<IPersonService, PersonService>();
 
-
 var app = builder.Build();
-
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
